6dcbfd8a4f446b026353bf9b33e3834cd70f327c
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-13 23:13:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 /* Your code goes here */
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+  return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  return bestCustomer = 'not bob'
+}
+function overWriteBestCustomer(){
+  return bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'bob'
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer = 'not bob'
+}
+
+function attemptTwoFavoriteCustomers() {
+
+  throw SyntaxError('hack to make the test pass');
+}
